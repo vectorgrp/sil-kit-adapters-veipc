b8ae37431f6c8a2e748f2f9a9a539b7971dd2020
Ismaeel AL NAJAR <ismaeel.alnajar@vector.com>
2025-10-24 14:46:05 +0200

    SKA-901: Introducing datagram pub sub adapter

---
+++ adapter/SilKitAdapterVEIPC.cpp
@@ -0,0 +1,122 @@
+// SPDX-FileCopyrightText: Copyright 2025 Vector Informatik GmbH
+// SPDX-License-Identifier: MIT
+
+#include "SilKitAdapterVEIPC.hpp"
+
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include <regex>
+
+#include <asio.hpp>
+
+#include "silkit/util/serdes/Serialization.hpp"
+#include "silkit/services/pubsub/all.hpp"
+
+#include "common/Cli.hpp"
+#include "common/Parsing.hpp"
+#include "common/ParticipantCreation.hpp"
+#include "common/SocketToDatagramPubSubAdapter.hpp"
+
+using namespace adapters;
+using namespace util;
+
+const std::string adapters::defaultParticipantName = "SilKitAdapterVEIPC";
+
+void print_help(bool userRequested = false)
+{
+    std::cout << "Usage (defaults in curly braces if you omit the switch):" << std::endl
+        << "sil-kit-adapter-veipc <host>:<port>,<toAdapterTopic>,<fromAdapterTopic> [" << participantNameArg
+        << " <participant's name{"<< defaultParticipantName <<"}>]" "\n"
+        "  [" << configurationArg << " <path to .silkit.yaml or .json configuration file>]\n"
+        "  [" << regUriArg << " silkit://<host{localhost}>:<port{8501}>]" "\n"
+        "  [" << logLevelArg << " <Trace|Debug|Warn|{Info}|Error|Critical|Off>]" "\n"
+        "  [" << endiannessArg << " <big|{little}>]" "\n"
+        "\nThe first positional argument is mandatory and must be of the form <host>:<port>,<toAdapterTopic>,<fromAdapterTopic>.\n"
+        "SIL Kit-specific CLI arguments will be overwritten by the config file passed by "
+        << configurationArg << ".\n";
+
+    std::cout << "\nExample:\n"
+        "sil-kit-adapter-veipc 192.168.1.3:6666,toSocket,fromSocket " << endiannessArg << " little\n";
+
+    if (!userRequested)
+        std::cout << "\nPass " << helpArg << " to get this message.\n";
+};
+
+int main(int argc, char** argv)
+{
+    if (findArg(argc, argv, helpArg, argv) != NULL)
+    {
+        print_help(true);
+        return CodeSuccess;
+    }
+
+    asio::io_context ioContext;
+    try
+    {
+        // Parse endianness switch before creating participant (default little_endian)
+        std::string endiannessStr = getArgDefault(argc, argv, endiannessArg, "little_endian");
+        adapters::utils::Endianness endianness = adapters::utils::Endianness::little_endian;
+        if (endiannessStr == "big_endian")
+            endianness = adapters::utils::Endianness::big_endian;
+        else if (endiannessStr != "little_endian")
+        {
+            std::cerr << "Invalid endianness value '" << endiannessStr << "'. Expected 'big_endian' or 'little_endian'." << std::endl;
+            throw InvalidCli();
+        }
+
+        const std::array<const std::string*,5> switchesWithArg = { &endiannessArg, &regUriArg, &logLevelArg, &participantNameArg, &configurationArg };
+        const std::array<const std::string*,1> switchesWithoutArg = { &helpArg };
+
+        // Collect positional socket specifications
+        auto socketSpecs = adapters::util_help::CollectPositionalSocketArgs(argc, argv, switchesWithArg, switchesWithoutArg);
+
+        // Create SIL Kit participant and services
+        SilKit::Services::Logging::ILogger* logger;
+        SilKit::Services::Orchestration::ILifecycleService* lifecycleService;
+        std::promise<void> runningStatePromise;
+        std::string participantName = defaultParticipantName;
+        const auto participant = CreateParticipant(argc, argv, logger, &participantName, &lifecycleService, &runningStatePromise);
+
+        // Instantiate socket adapters
+        std::vector<std::unique_ptr<bytes_socket::SocketToDatagramPubSubAdapter>> transmitters;
+        std::set<std::string> alreadyProvidedSockets;
+        const uint8_t headerSize = 2; // default typical size field length
+        for (auto spec : socketSpecs)
+        {
+            transmitters.emplace_back(bytes_socket::SocketToDatagramPubSubAdapter::parseArgument(
+                spec, alreadyProvidedSockets, participantName, ioContext, participant.get(), endianness, headerSize, logger));
+        }
+
+        auto finalStateFuture = lifecycleService->StartLifecycle();
+
+        std::thread ioContextThread([&]() { ioContext.run(); });
+
+        promptForExit();
+
+        Stop(ioContext, ioContextThread, *logger, &runningStatePromise, lifecycleService, &finalStateFuture);
+    }
+    catch (const SilKit::ConfigurationError& error)
+    {
+        std::cerr << "Invalid configuration: " << error.what() << std::endl;
+        return CodeErrorConfiguration;
+    }
+    catch (const InvalidCli&)
+    {
+        print_help();
+        std::cerr << std::endl << "Invalid command line arguments." << std::endl;
+        return CodeErrorCli;
+    }
+    catch (const SilKit::SilKitError& error)
+    {
+        std::cerr << "SIL Kit runtime error: " << error.what() << std::endl;
+        return CodeErrorOther;
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "Something went wrong: " << error.what() << std::endl;
+        return CodeErrorOther;
+    }
+
+    return CodeSuccess;
+}
---
+++ adapter/SilKitAdapterVEIPC.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) Vector Informatik GmbH. All rights reserved.
+
+#pragma once
+
+#include <string>
+
+namespace adapters {
+
+    extern const std::string endiannessArg;
+    extern const std::string defaultParticipantName;
+
+}
