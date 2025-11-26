fb605e03042e8e800a2b8701a8ffafcf03bce507
Al Najar, Ismaeel <Ismaeel.AlNajar@vector.com>
2025-11-26 16:49:15 +0100

    SKA-901: removed out_dir

--- demos/CANoe4SW_SE/createEnvironment.sh
+++ demos/CANoe4SW_SE/createEnvironment.sh
@@ -3,15 +3,14 @@
 # SPDX-License-Identifier: MIT
 
 script_root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-out_dir="${1:-$script_root}"
 
 echo "[info] Creating environment"
-$canoe4sw_se_install_dir/environment-make $script_root/venvironment.yaml -o "$out_dir" -A "Linux64"
+$canoe4sw_se_install_dir/environment-make $script_root/venvironment.yaml -o "$script_root" -A "Linux64"
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     exit $exit_status
 fi
 
 echo "[info] Compiling tests"
-$canoe4sw_se_install_dir/test-unit-make $script_root/../tests/testEchoServer.vtestunit.yaml -e $out_dir/Default.venvironment -o "$out_dir"
+$canoe4sw_se_install_dir/test-unit-make $script_root/../tests/testEchoServer.vtestunit.yaml -e $script_root/Default.venvironment -o "$script_root"
 exit $?
