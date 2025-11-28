b8067fb8c43761fbb30068418a62886c4114a305
kaubry <kyllian.aubry@vector.com>
2025-11-28 11:49:14 +0100

    SKA-925: handle pids in run_all.sh script

--- demos/CANoe4SW_SE/run_all.sh
+++ demos/CANoe4SW_SE/run_all.sh
@@ -2,6 +2,10 @@
 # SPDX-FileCopyrightText: Copyright 2025 Vector Informatik GmbH
 # SPDX-License-Identifier: MIT
 
+echo "-----------------------------------------------------------------------"
+ps aux | grep sil
+echo "-----------------------------------------------------------------------"
+
 # check if user is root
 if [[ $EUID -ne 0 ]]; then
   echo "[error] This script must be run as root / via sudo!"
@@ -10,7 +14,7 @@ fi
 
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 logdir=$scriptDir/logs
-silKitDir=/home/dev/SilKit-5.0.1-ubuntu-22.04-x86_64-gcc/
+silKitDir=/home/dev/vfs/SILKit/SilKit-5.0.1-ubuntu-22.04-x86_64-gcc/
 # if "exported_full_path_to_silkit" environment variable is set (in pipeline script), use it. Otherwise, use default value
 silKitDir="${exported_full_path_to_silkit:-$silKitDir}"
 
@@ -35,10 +39,12 @@ timeout 30s grep -q 'Press Ctrl-C to terminate...' <(tail -f $logdir/sil-kit-reg
 # run the tests with little_endian configuration
 echo "[info] Starting echo server (little_endian)"
 $scriptDir/../../bin/sil-kit-demo-veipc-echo-server --endianness little_endian &> $logdir/sil-kit-demo-veipc-echo-server_little_endian_$timestamp.out &
+demo_id=$!
 
 echo "[info] Starting the adapter (little_endian)"
 $scriptDir/../../bin/sil-kit-adapter-veipc localhost:6666,toSocket,fromSocket --endianness little_endian \
   &> $logdir/sil-kit-adapter-veipc_little_endian_$timestamp.out &
+adapter_id=$!
 echo "[info] Starting run.sh test script"
 $scriptDir/run.sh
 res=$?
@@ -52,13 +58,20 @@ else
   exit_status=1
 fi
 
+# cleanup little_endian processes
+echo "[info] Cleaning up little_endian processes"
+kill -2 $demo_id $adapter_id &>/dev/null
+sleep 1 # wait for processes to terminate
+
 # run the tests with big_endian configuration
 echo "[info] Starting echo server (big_endian)"
 $scriptDir/../../bin/sil-kit-demo-veipc-echo-server --endianness big_endian &> $logdir/sil-kit-demo-veipc-echo-server_big_endian_$timestamp.out &
+demo_id=$!
 
 echo "[info] Starting the adapter (big_endian)"
 $scriptDir/../../bin/sil-kit-adapter-veipc localhost:6666,toSocket,fromSocket --endianness big_endian \
   &> $logdir/sil-kit-adapter-veipc_big_endian_$timestamp.out &
+adapter_id=$!
 echo "[info] Starting run.sh test script"
 $scriptDir/run.sh
 res=$?
@@ -69,6 +82,11 @@ else
   echo "[info] Tests failed (big_endian)"
   exit_status=1
 fi
+
+# cleanup big_endian processes
+echo "[info] Cleaning up big_endian processes"
+kill -2 $demo_id $adapter_id &>/dev/null
+sleep 1 # wait for processes to terminate and flush logs
 
 # exit run_all.sh with same exit_status
 exit $exit_status
